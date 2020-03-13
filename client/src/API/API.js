79be434dd3113afa790a7d6e4f4a99c7bf03ed7d
Jesse Schimmel <jhowardschimmel@gmail.com>
2020-03-13 00:55:24 -0400

    Mess with useEffect hook

--- client/src/API/API.js
+++ client/src/API/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default {
-  searchTerms: function(query) {
+  getWeather: function() {
     return axios.get(
       "api.openweathermap.org/data/2.5/weather?q=roswell,georgia&appid=fff4bd82a86c47ebcf45b2eaa19a0e8d"
     );
--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -3,15 +3,31 @@ import API from "../../API/API.js";
 
 
 function Card(props) {
+    const [weatherObject, setWeatherObject] = useState({})
     
+    useEffect(() => {
+        API.getWeather().then((res) => {
+
+            // const weather = {
+            //     localWeather: res.weather[0].main,
+            //     city: res.city
+            // };
+            setWeatherObject(res.weatherObject)
+        });
+    }, [])
+
     return (
         <div>
                 <div className="card">
                     <img src="https://i.picsum.photos/id/1000/200/200" alt="random" >
                     </img>
                     <div className="container">
-                        <h4><b>John Doe</b></h4>
-                        <p>Architect & Engineer</p>
+                        <h4><b>{weatherObject.map(item => (
+                            <li key={item.objectID}>
+                                <p>{item.name}</p>
+                            </li>
+                        ))}</b></h4>
+                        <p>weather</p>
     
 
                     </div>
